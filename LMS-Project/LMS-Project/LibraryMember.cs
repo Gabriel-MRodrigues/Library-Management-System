9ddf8879b1cad23f24982024f97c02bcf82e5fe7
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-01-17 15:58:35 -0400

    Added LibraryMember class. Properties and Constructors

---
+++ LMS-Project/LMS-Project/LibraryMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_Project
+{
+    internal class LibraryMember
+    {
+        public int MemberID { get; set; }
+        public string Name { get; set; }
+        public List<string> BooksBorrowed { get; set; }
+
+        public LibraryMember() { }
+        public LibraryMember(int MemberID, string Name)
+        {
+            this.MemberID = MemberID;
+            this.Name = Name;
+            BooksBorrowed = new List<string>();
+        }
+    }
+}
