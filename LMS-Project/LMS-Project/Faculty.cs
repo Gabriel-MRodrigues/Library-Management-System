d46ccc26837bbea801f84f9628e48f0bf426ac8e
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-01-17 16:14:58 -0400

    Added Property and Constructors

--- LMS-Project/LMS-Project/Faculty.cs
+++ LMS-Project/LMS-Project/Faculty.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace LMS_Project
 {
-    internal class Faculty
+    internal class Faculty : LibraryMember
     {
+        public string Department { get; set; }
+        public Faculty() { }
+        public Faculty(int MemberID, string Name, string Department)
+            : base(MemberID, Name)
+        {
+            this.Department = Department;
+        }
     }
 }
