ea57af6c1a3ec54282342069848a192551a4c21a
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-01-17 16:24:37 -0400

    Created simple GUI for user interact

--- LMS-Project/LMS-Project/Program.cs
+++ LMS-Project/LMS-Project/Program.cs
@@ -10,6 +10,41 @@ namespace LMS_Project
     {
         static void Main(string[] args)
         {
+            void askQuestions()
+            {
+                Console.WriteLine("\n1. Add New Member.");
+                Console.WriteLine("2. Borrow or Return Book.");
+                Console.WriteLine("3. View Details.");
+                Console.WriteLine("4. Exit\n");
+                Console.WriteLine("Press a number to take action.\n");
+            }
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+                askQuestions();
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        isRunning = false;
+                        Console.WriteLine("Exiting the program... press enter.");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid enter. Try again...");
+                        break;
+                }
+            }
         }
     }
 }
