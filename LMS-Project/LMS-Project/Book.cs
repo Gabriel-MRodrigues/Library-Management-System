65c9a56de7979a4aa4a6d312200550fcab22174c
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-02-13 12:56:19 -0400

    Fixed some bugs from the book class

--- LMS-Project/LMS-Project/Book.cs
+++ LMS-Project/LMS-Project/Book.cs
@@ -11,7 +11,7 @@ namespace LMS_Project
     internal class Book
     {
         public string Title {  get; set; }
-        public string Description { get; set; } = "No description found";
+        public string Description { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
         public bool isAvailable { get; set; }
@@ -38,6 +38,9 @@ namespace LMS_Project
             if (parts.Length != 5)
                 throw new FormatException("Invalid book format in file.");
 
+            if (parts[3].Trim() == "")
+                parts[3] = "No Descriptiono found for this book.";
+
             return new Book(
                    parts[0], // Title
                    parts[1], // Author
@@ -45,6 +48,11 @@ namespace LMS_Project
                    parts[3], // Description
                    bool.Parse(parts[4]) // IsAvailable
                 );
+        }
+
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine($"-----------\n- {Title} by {Author}\n Description: {Description}\n ISBN: {ISBN}\n \t - {(isAvailable ? "Is Available" : "Not Available")}");
         }
 
     }
